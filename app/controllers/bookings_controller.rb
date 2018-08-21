a25a4998a24d1b2fcc4a35358ccac4ce8c786260
Oliver Marriott <olimarrio@gmail.com>
2018-08-21 12:23:43 +0100

    fixed routing issues and otther assorted titbits

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,55 @@
 class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show, :edit, :destroy, :new]
+
+  def index
+    @bookings = Booking.all
+  end
+
   def show
   end
 
   def new
+    @booking = Booking.new
   end
 
   def edit
   end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @booking = booking.new(booking_params)
+    @booking.user = @user
+    if
+      @booking.save
+      redirect_to bookings_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = @booking.user
+    @booking.destroy
+    redirect_to booking_path(@user)
+  end
+
+
+private
+
+def find_booking
+  @booking = Booking.find(params[:id])
 end
+
+
+def booking_params
+  params.require(:booking).permit(:user_id, :garden_id, :date)
+end
+
+
+end
+
