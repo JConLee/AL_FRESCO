6a68a0eba9cb1d99e9402d54e637562beccca82d
Jess Lee <jessclee92@me.com>
2018-08-21 11:18:43 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all the users and gardens..."
+
+Garden.destroy_all
+User.destroy_all
+
+puts "Creating users and gardens"
+
+user_1 = User.create(first_name: 'Jess', last_name: 'Lee', email: "jess@gmail.com", password: "123456")
+user_2 = User.create(first_name: 'Ben',last_name: 'Herring', email: "ben@gmail.com", password: "789110")
+user_3 = User.create(first_name: 'Luiza' ,last_name: 'Pinto', email: "luiza@gmail.com", password: "119567")
+user_4 = User.create(first_name: 'Oli', last_name: 'Marriott', email: "oli@gmail.com", password: "341567")
+
+# puts "#{user.errors.messages}"
+
+garden_1 = Garden.create(title: 'Walled Garden', address: 'Devon' )
+garden_2 = Garden.create(title: 'Pool Garden' , address: 'Portugal')
+garden_3 = Garden.create(title: 'Maze Garden',address: 'East Sussex')
+garden_4 =  Garden.create(title: 'Historic woodland', address: 'Hampshire')
+
+garden_1.user = user_1
+garden_1.save
+
+garden_2.user = user_2
+garden_2.save
+
+garden_3.user = user_3
+garden_3.save
+
+garden_4.user = user_4
+garden_4.save
+
+
+
+
+# puts "#{garden.errors.messages}"
+
+puts "Created #{Garden.count} gardens and #{User.count} users."
+
+
