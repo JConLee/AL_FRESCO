a319b78b66720b3bb6e53734d1384a3632cde816
Oliver Marriott <olimarrio@gmail.com>
2018-08-23 12:00:56 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :gardens, only: [:new, :create, :edit, :update]
   end
-  resources :gardens, only: [:index, :show] do
+  resources :gardens, only: [:index, :show, :destroy] do
   resources :bookings
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
