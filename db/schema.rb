67ff3c8d74d3dd087d7f0a1bc2884a30cfb73247
Ben Herring <ben_herring@hotmail.com>
2018-08-23 16:08:54 +0100

    added image to garden show page and price and description fields for new garden

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_132300) do
+ActiveRecord::Schema.define(version: 2018_08_23_141921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_08_22_132300) do
     t.float "latitude"
     t.float "longitude"
     t.string "photo"
+    t.integer "price"
+    t.text "description"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
