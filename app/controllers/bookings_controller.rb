ba392f23d5e8a0b56138a31cac5c702b09f0a60c
Ben Herring <ben_herring@hotmail.com>
2018-08-24 13:41:02 +0100

    book confirmation page image added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,7 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @garden = @booking.garden
   end
 
   def new
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,4 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 
-// import mapElement from './maps.js'
-// import 'maps';
 
